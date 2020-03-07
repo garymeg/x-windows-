95536a191232fbae5e41bbbf75d7f2626dff790e
garymeg <github@garymeg.co.uk>
2020-03-07 16:08:02 +0000

    wrong folder

--- 3.install twin db9-2-usb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# File to Make Patition 2 on usbstick hold games and system files
-# 
-# Copy the system files to mount the USB
--- 4.Install new carousel games.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# File to Make Patition 2 on usbstick hold games and system files
-# 
-# Copy the system files to mount the USB
-
-cp -r /media/usb/CarouselGames/games/ /media/usb2/usr/share/the64/
-cp -r /media/usb/menu.wav /media/usb2/usr/share/the64/ui/sounds/
-echo Games added......
